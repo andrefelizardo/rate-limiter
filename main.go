566416649713c0feb629446efe4ba1c830085803
andrefelizardo <andreluis.felizardo@outlook.com>
2026-07-12 11:45:39 -0300

    Add go.mod file and implement default test endpoint with simulated latency

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
@@ -13,14 +15,29 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "This is my website!\n")
 }
 
-func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / hello request\n")
-	io.WriteString(w, "Hello, HTTP!\n")
+func getDefaultTest(w http.ResponseWriter, r *http.Request) {
+	startTime := time.Now()
+	isSlow := rand.Float64() < 	0.1
+
+	var sleepDuration time.Duration
+	if isSlow {
+		sleepDuration = time.Duration(1000+rand.Intn(2000)) * time.Millisecond
+	} else {
+		sleepDuration = time.Duration(10+rand.Intn(90)) * time.Millisecond
+	}
+
+	time.Sleep(sleepDuration)
+
+	elapsedTime := time.Since(startTime)
+
+	fmt.Printf("got / default test request\n")
+	fmt.Fprintf(w, "elapsed_time: %d", elapsedTime)
+	io.WriteString(w, "\nTest call\n")
 }
 
 func main() {
 	http.HandleFunc("/", getRoot)
-	http.HandleFunc("/hello", getHello)
+	http.HandleFunc("/test", getDefaultTest)
 
 	err := http.ListenAndServe(":3333", nil)
 
